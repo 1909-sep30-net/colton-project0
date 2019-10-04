d8ff84f50fbebddd380afcd63e7f34369ea4ad5c
Colton Clary <coltonclary0806@yahoo.com>
2019-10-04 13:32:53 -0500

    worked on order logic

--- StoreApplication/BusinessLogic.Library/Order.cs
+++ StoreApplication/BusinessLogic.Library/Order.cs
@@ -4,8 +4,39 @@ using System.Text;
 
 namespace BusinessLogic.Library
 {
-    class Order
+    public class Order
     {
+        //List<Product> ProductNames;
+        Product ProductName;
+
+        double Total;
+        int Quantity;
+        double Price;
+        public Order(Product productname, string location, )
+        {
+
+        }
+         
+
+        public void CreateNewOrder(Product name)
+        {
+            ordernames.Add(name);
+            
+        }
+
+        public double Calculate()
+        {
+            return Quantity * Price;
+        }
+
+
+        public class ModifyOrder
+        {
+
+        }
 
     }
 }
+
+
+
