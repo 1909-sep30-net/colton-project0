1cc6235caca88dd9517dda3baefceab79d7f7f9b
Colton Clary <coltonclary0806@yahoo.com>
2019-10-04 10:24:49 -0500

    first push for starters

---
+++ StoreApplication/BusinessLogic.Library/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BusinessLogic.Library
+{
+    public class Customer
+    {
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string location { get;  set; }
+
+        public void AddNewCustomer()
+        {
+
+        }
+    }
+
+}
